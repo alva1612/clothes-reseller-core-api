bb4fd054c809c1f4cced5be721322e83dc4ecc15
Alvaro <alvaro.guillenc1612@gmail.com>
2023-11-19 23:05:27 -0500

    swagger added

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RegisterDto {
+  @ApiProperty({ example: 'Alvaro Axel' })
   name: string;
+  @ApiProperty({ example: 'Guillén Carbajal' })
   lastName: string;
+  @ApiProperty({ example: 'aguillen' })
   username: string;
+  @ApiProperty({ example: 'wowpassword123*' })
   password: string;
+  @ApiProperty({ example: 'wowpassword123*' })
   confirmPassword: string;
+  @ApiProperty({ example: 'alvaro.guillenc1612@gmail.com' })
   email: string;
+  @ApiProperty({ example: '947540679' })
   phoneNumber: string;
 }
